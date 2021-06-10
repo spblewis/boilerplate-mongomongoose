bb99c681eb6d55e43c5b6677072914cc64eaa6a6
spblewis <spblewis@gmail.com>
2021-06-10 11:35:20 -0500

    fixed spelling error

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let Person;
 
