d63769da1065eb1a046a3acb545e4ffb2d20778b
spblewis <spblewis@gmail.com>
2021-06-10 11:26:39 -0500

    Added mongo and mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let Person;
 
@@ -51,20 +54,24 @@ const queryChain = (done) => {
   done(null /*, data*/);
 };
 
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
-
-//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
-
-exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
-exports.findPeopleByName = findPeopleByName;
-exports.findOneByFood = findOneByFood;
-exports.findPersonById = findPersonById;
-exports.findEditThenSave = findEditThenSave;
-exports.findAndUpdate = findAndUpdate;
-exports.createManyPeople = createManyPeople;
-exports.removeById = removeById;
-exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
+export const PersonModel = Person;
+const _createAndSavePerson = createAndSavePerson;
+export { _createAndSavePerson as createAndSavePerson };
+const _findPeopleByName = findPeopleByName;
+export { _findPeopleByName as findPeopleByName };
+const _findOneByFood = findOneByFood;
+export { _findOneByFood as findOneByFood };
+const _findPersonById = findPersonById;
+export { _findPersonById as findPersonById };
+const _findEditThenSave = findEditThenSave;
+export { _findEditThenSave as findEditThenSave };
+const _findAndUpdate = findAndUpdate;
+export { _findAndUpdate as findAndUpdate };
+const _createManyPeople = createManyPeople;
+export { _createManyPeople as createManyPeople };
+const _removeById = removeById;
+export { _removeById as removeById };
+const _removeManyPeople = removeManyPeople;
+export { _removeManyPeople as removeManyPeople };
+const _queryChain = queryChain;
+export { _queryChain as queryChain };
