8fe8bb216288d579289fcf9d833b510c49af2b9d
spblewis <spblewis@gmail.com>
2021-06-30 09:21:23 -0500

    Added one to delete by id

--- myApp.js
+++ myApp.js
@@ -76,7 +76,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, person) => {
+    if (err) console.error(err);
+    done(null, person);
+  });
 };
 
 const removeManyPeople = (done) => {
